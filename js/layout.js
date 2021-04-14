585bced04a415d336d4cd5129941a72c04ac5afc
Tiennd0502 <Tiennd0502@gmail.com>
2021-04-14 09:56:27 +0700

    add file json and view page:

--- js/layout.js
+++ js/layout.js
@@ -2,26 +2,39 @@ let myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(($routeProvider) => {
   $routeProvider.when('/index', {
-      templateUrl: './layouts/index.html?' + Math.random(),
-      controller: 'SubjectCtrl'
-    }).when('/introduce', {
-      templateUrl: './layouts/introduce.html?' + Math.random()
-    }).when('/contact', {
-      templateUrl: '/layouts/contact.html?' + Math.random()
-    }).when('/feedback', {
-      templateUrl: '/layouts/feedback.html?' + Math.random()
-    }).when('/answer', {
-      templateUrl: '/layouts/answer.html?' + Math.random()
-    }).when('/subject', {
-      templateUrl: '/layouts/subject.html?' + Math.random(),
-      controller: 'SubjectCtrl'
-    }).when('/quiz/:id/:name/:logo', {
-      templateUrl: '/layouts/quiz.html?' + Math.random(),
-      controller: 'QuizCtrl'
-    })
-    .otherwise({
-      redirectTo: '/index'
-    });
+    templateUrl: './layouts/index.html?' + Math.random(),
+    controller: 'SubjectCtrl'
+  }).when('/introduce', {
+    templateUrl: './layouts/introduce.html?' + Math.random()
+  }).when('/contact', {
+    templateUrl: '/layouts/contact.html?' + Math.random()
+  }).when('/feedback', {
+    templateUrl: '/layouts/feedback.html?' + Math.random()
+  }).when('/answer', {
+    templateUrl: '/layouts/answer.html?' + Math.random()
+  }).when('/subject', {
+    templateUrl: '/layouts/subject.html?' + Math.random(),
+    controller: 'SubjectCtrl'
+  }).when('/quiz/:id/:name/:logo', {
+    templateUrl: '/layouts/quiz.html?' + Math.random(),
+    controller: 'QuizCtrl'
+  }).when('/login', {
+    templateUrl: '/layouts/login.html?' + Math.random(),
+    controller: 'LoginCtrl'
+  }).when('/regis', {
+    templateUrl: '/layouts/regis.html?' + Math.random(),
+    controller: 'RegisCtrl'
+  }).when('/forgotPass', {
+    templateUrl: '/layouts/forgot_pass.html?' + Math.random(),
+    controller: 'ForgotPassCtrl'
+  }).when('/logout', {
+    templateUrl: '/layouts/logout.html?' + Math.random(),
+    controller: 'LogoutCtrl'
+  }).when('/error', {
+    templateUrl: '/layouts/error.html?' + Math.random(),
+  }).otherwise({
+    redirectTo: '/index'
+  });
 });
 
 myApp.run(($rootScope) => {
@@ -36,33 +49,78 @@ myApp.run(($rootScope) => {
     alert('Load trang không thành công!');
   });
 });
-myApp.controller('MasterCtrl', function($scope, $http) {
-  $scope.list_subject = [];
-  $scope.length = 0;
-  $http.get('../db/Subjects.js').then((res) => {
-    $scope.list_subject = res.data;
-  }, (res) => {
-    console.log("k load được data");
-  });
-  $scope.currentPage = 0;
-  $scope.defaultPageSize = 9;
-  $scope.pageSize = 9;
-  $scope.data = [];
+myApp.controller('MasterCtrl', function($scope, $rootScope, $http) {
+    $scope.list_subject = [];
+    $scope.length = 0;
+    $http.get('../db/Subjects.js').then((response) => {
+      $scope.list_subject = response.data;
+    }, (error) => {
+      console.log("k load được data");
+    });
+    $scope.currentPage = 0;
+    $scope.defaultPageSize = 9;
+    $scope.pageSize = 9;
+    $scope.data = [];
 
-  $scope.numberOfPages = () => {
-    let len = Math.ceil($scope.data.length / $scope.pageSize);
-    $scope.pageArray = [];
-    for (let i = 0; i < len; i++) {
-      $scope.pageArray.push(i);
+    $scope.numberOfPages = () => {
+      let len = Math.ceil($scope.data.length / $scope.pageSize);
+      $scope.pageArray = [];
+      for (let i = 0; i < len; i++) {
+        $scope.pageArray.push(i);
+      }
+      return len;
     }
-    return len;
+    for (let i = 0; i < 21; i++) {
+      $scope.data.push("Item " + i);
+    }
+    if (typeof localStorage.getItem('user') === 'undefined' || localStorage.getItem('user') === null) {
+      $rootScope.login = true;
+    }
+
+  })
+  // login controller
+myApp.controller('LoginCtrl', function($scope, $rootScope, $http) {
+  //  check login
+  $scope.urlUser = 'http://localhost:3000/users';
+  $scope.error = false;
+
+
+  $scope.checkLogin = (username, password) => {
+    let urlLogin = `${$scope.urlUser}?username=${username}&password=${password}`;
+    $http.get(urlLogin).then((response) => {
+      let user = response.data;
+      let check = response.data.length;
+      if (check) {
+        localStorage.setItem('user', JSON.stringify(user[0]));
+        window.location.href = 'index.html#!/subject';
+        $rootScope.login = false;
+      } else {
+        localStorage.setItem('user', '');
+        $rootScope.login = true;
+        $scope.error = true;
+      }
+    }, (error) => {
+      console.log("k load được data");
+    });
   }
-  for (let i = 0; i < 21; i++) {
-    $scope.data.push("Item " + i);
-  }
-  $scope.login = true;
-})
-myApp.directive('quizfpoly', function($interval, $quizFactory, $routeParams) {
+});
+
+// regis
+myApp.controller('RegisCtrl', function($scope, $rootScope, $http) {
+  //  
+  $scope.urlUser = 'http://localhost:3000/users';
+});
+// Forgot pass
+myApp.controller('ForgotPassCtrl', function($scope, $rootScope, $http) {
+
+});
+// Forgot pass
+myApp.controller('LogoutCtrl', function($scope, $rootScope, $http) {
+  $rootScope.login = true;
+  localStorage.clear();
+});
+// 
+myApp.directive('quizfpoly', function($http, $interval, $quizFactory, $routeParams) {
   return {
     restrict: "E",
     scope: [],
@@ -90,25 +148,29 @@ myApp.directive('quizfpoly', function($interval, $quizFactory, $routeParams) {
       scope.name = $routeParams.name;
       scope.id = $routeParams.id;
       scope.logo = $routeParams.logo;
-
+      scope.urlHistory = 'http://localhost:3000/histories';
       scope.start = () => {
-        $quizFactory.LoadQuestions().then(() => {
-          // thời gian làm bài
-          scope.strTime = '';
-          scope.setTime();
-          scope.time = true;
-          scope.showQuiz = true;
-          scope.endQuiz = false;
-          scope.questionNumber = 15;
-          scope.marks = {};
-          scope.mark = 0;
-          scope.totalScore = 0;
-          scope.answersTrue = {};
-          scope.answers = {};
-          appendHtml(scope.questionNumber);
-        });
+        if (!localStorage.getItem('user') == '') {
+          $quizFactory.LoadQuestions().then(() => {
+            // thời gian làm bài
+            scope.strTime = '';
+            scope.setTime();
+            scope.time = true;
+            scope.showQuiz = true;
+            scope.endQuiz = false;
+            scope.questionNumber = 15;
+            scope.marks = {};
+            scope.mark = 0;
+            scope.totalScore = 0;
+            scope.answersTrue = {};
+            scope.answers = {};
+            appendHtml(scope.questionNumber);
+          });
+        } else {
+          window.location.href = 'index.html#!/login';
+        }
       };
-      // check before submit
+      // check before finish
       scope.checkSubmit = 'false';
       scope.checkFinish = () => {
         if (scope.checkSubmit) {
@@ -118,7 +180,7 @@ myApp.directive('quizfpoly', function($interval, $quizFactory, $routeParams) {
         }
       };
 
-      // kết thúc quiz
+      // finish quiz
       scope.endTest = () => {
         scope.endQuiz = true;
         scope.showQuiz = false;
@@ -129,12 +191,29 @@ myApp.directive('quizfpoly', function($interval, $quizFactory, $routeParams) {
         }
         $interval.cancel(scope.timer);
         scope.totalScore = (scope.mark * 10 / scope.questionNumber).toFixed(2);
+        scope.saveQuiz();
       };
-      // làm mới quiz
+      // save quiz
+      scope.saveQuiz = () => {
+          let user = JSON.parse(localStorage.getItem('user'));
+          let data = {
+            user_id: user.id,
+            subject: $routeParams.name,
+            marks: scope.totalScore,
+          };
+          $http.post(scope.urlHistory, data).then((response) => {
+            console.log(response.data);
+
+          }, (error) => {
+            console.log(error);
+          });
+        }
+        // reset quiz
       scope.reset = () => {
         scope.showQuiz = false;
         scope.endQuiz = false;
       };
+      // html entity
       scope.htmlEntities = (str) => {
         const htmlEntities = {
           "&": "&amp;",
@@ -158,7 +237,7 @@ myApp.directive('quizfpoly', function($interval, $quizFactory, $routeParams) {
           _inner += `<div class="carousel-item ${(key ==0)? 'active': ""}">
                       <div class="question pb-3">
                         <strong ng-bind="index">
-                          ${Number(key)+1}. ${questions[key].Text}
+                          ${Number(key)+1}. ${scope.htmlEntities(questions[key].Text)}
                         </strong>
                       </div>
                       <div class="answer mt-2 pb-3 mb-2">`;
